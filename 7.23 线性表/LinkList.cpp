31a2ef8dc39a8a031f8585de45b04ae0bf888141
菜鸟 <56874062+clowning@users.noreply.github.com>
2021-07-24 08:00:54 +0800

    添加一个链表

---
+++ 7.23 线性表/LinkList.cpp
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+typedef struct lNode 
+{
+    int data;
+    struct LNode *next;
+}*LinkList,lNode;
+LNode * GetElemByLocation(LinkList L,int i){
+    int  j = 1;
+    LNode *p ;
+    return p;
+}
+
+
+int main(){
+    return 0;
+}
