cc8a0b8ac20d16056725f51059158c5ff7845e8a
菜鸟 <56874062+clowning@users.noreply.github.com>
2021-09-15 19:41:19 +0800

    给顺序表添加插入和删除两个方法

--- 线性表/顺序表.cpp
+++ 线性表/顺序表.cpp
@@ -16,10 +16,43 @@ void InitList(SqList &L){
     }
     L.length=0;//顺序表初始长度为0
 }
+
+bool ListDelete(SqList &L,int i,int &e){
+    if(i<1||i>L.length+1)//判断删除位置是否合法
+        return false;
+    e = L.data[i-1];//取出原来的第i个元素
+    for (int j = i; j < L.length; j++)
+    {
+        L.data[j-1]=L.data[j];//把i以后的元素往前移一位，并覆盖原来的第i个元素
+    }
+    L.length--;//顺序表长度减一
+    return true;
+    
+}
+
+
+bool ListInsert(SqList &L,int i,int e){
+    if(i<1||i>L.length+1)//判断插入位置是否合法
+        return false;
+    if(L.length>=MAXSIZE)//判断插入位置是否合法
+        return false;
+    for (int  j = L.length; j >=i; j--)
+    {
+        L.data[j]= L.data[j-1];//把i以后的所有元素全部往后移一位
+
+    }
+        L.data[i-1]=e;//给第i个元素赋值
+        L.length++;//顺序表长度加一
+    return true;
+}
 int main(){
+    int e;
     SqList L;
     InitList(L);
-    for (int i = 0; i < MAXSIZE; i++)
+    ListInsert(L,1,3);
+    ListInsert(L,2,4);
+    ListDelete(L,1,e);
+    for (int i = 0; i < L.length; i++)
     {
         printf("data[%d]=%d\n",i,L.data[i]);
     }
